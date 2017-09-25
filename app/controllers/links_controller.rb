43079fd84581100ab3a62b3a1ae321706173fc53
jakedub <jacob.w.moore@gmail.com>
2017-09-24 21:05:18 -0400

    upvote works, downvote does not

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -11,60 +11,49 @@ class LinksController < ApplicationController
   # GET /links/1
   # GET /links/1.json
   def show
+    @links = Link.find(params[:id])
   end
 
   # GET /links/new
   def new
-    @link = Link.new
+    @links = Link.new
   end
 
   # GET /links/1/edit
   def edit
+    @links = Link.find(params[:id])
   end
 
   # POST /links
   # POST /links.json
   def create
-    @link = Link.new(link_params)
-    @link.user = current_user
-    @link.totalcount = @link.votes.count
-      if @link.save
-        redirect_to @link
+    @links = Link.new(link_params)
+    @links.user = current_user
+    @links.totalcount = @links.votes.count
+      if @links.save
+        redirect_to @links
       else
         render :new
       end
-    respond_to do |format|
-      if @link.save
-        format.html { redirect_to @link, notice: 'Link was successfully created.' }
-        format.json { render :show, status: :created, location: @link }
-      else
-        format.html { render :new }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def vote
-    if current_user
-      @link.vote << Vote.create!(user_id: @link.user_id, link_id: @link.id)
-      @link.totalcount = @link.votes.count
-      @link.save
-      redirect_to :root
-    else
-      redirect_to :login
-    end
-  end
-
-  def link_vote
+    @link = Link.find(params[:link_id])
     @link.votes << Vote.create!(user_id: @link.user_id, link_id: @link.id)
     @link.totalcount = @link.votes.count
     @link.save
     redirect_to :root
   end
 
+  def link_vote
+    @links.totalcount = @links.votes.count
+    @links.save
+    redirect_to :root
+  end
+
   def down_vote
-    @link.votes.last.destroy
-    @link.totalcount = @link.votes.count
+    @link = Link.find(params[:link_id])
+    @link.totalcount = @link.votes.last.destroy
     @link.save
     redirect_to :root
   end
@@ -73,15 +62,15 @@ class LinksController < ApplicationController
   # PATCH/PUT /links/1
   # PATCH/PUT /links/1.json
   def update
-    @link.update(link_params)
-    @link.save
+    @links.update(link_params)
+    @links.save
     redirect_to :root
   end
 
   # DELETE /links/1
   # DELETE /links/1.json
   def destroy
-    @link.destroy
+    @links.destroy
     respond_to do |format|
       format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
       format.json { head :no_content }
@@ -91,11 +80,11 @@ class LinksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_link
-      @link = Link.find(params[:id])
+      @links = Link.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
-      params.require(:link).permit(:url, :title)
+      params.require(:link).permit(:url, :title, :link_id)
     end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,5 @@
 class Link < ApplicationRecord
   has_many :votes
   belongs_to :user
+  validates :url, :title, presence:true
 end
